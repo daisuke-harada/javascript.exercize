784e047d7da579e5669643bd15582c5da5662a6c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-01-08 23:36:43 +0900

    [fix] タブメニューバーの作成

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,13 +1,23 @@
-$(function(){
-  $('.menu-trigger').on('click', function(event){
-    // .toggleClass( )は、指定されているclass（ここではactive）をトグル処理するメソッドです。
-    // トグル処理では、該当のclass属性がある場合にはclassが削除され、ない場合にはclassが追加されます。
-    $(this).toggleClass('active');
+/*$()関数では、特定の属性を持つ要素や属性値を絞り込むことができます。*/
+/*id=tab-contents要素子供の要素であり、 class=tabが適用されている要素であり、id=tab1ではない要素が適用される。*/
+/* .hideは特定のHTML要素を非表示にする。
+$('#tab-contents .tab[id != "tab1"]').hide();
 
-    // .fadeToggle( )は、要素のフェードイン・フェードアウトを切り替えるメソッドです。
-    // このメソッドを使って、ハンバーガーメニューを開いたときの状態を設定します。
-    //ここでは、#sp-menuが開いたときに、.fadeToggle( )が処理されるように設定しています。
-    $('#sp-menu').fadeToggle();
+/*id=tab-menu要素の中のaのclassをクリックした時*/
+$('#tab-menu a').on('click', function(event){
+
+    //tab classた設定されている要素が一旦削除される。
+    $("#tab-contents .tab").hide();
+
+    // activeクラスが削除される。
+    $("#tab-menu .active").removeClass("active");
+
+    // クリックされたものにactiveクラスが適用される。
+    $(this).addClass("active");
+
+    // .attr()は、HTML要素の属性を取得したり設定できるメソッドです。
+    // .show()は要素を表示するメソッドである。
+    // 取得したhref属性の中身が表示される。
+    $($(this).attr("href")).show();
     event.preventDefault();
-  });
 });
